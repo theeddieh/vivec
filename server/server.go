0788e0ddde6a08d52b515fe38c83a4a303311e9e
theeddieh <theeddieharrison@gmail.com>
2016-04-15 00:00:35 +0000

    #1: added request parsing

--- server/server.go
+++ server/server.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -10,7 +11,21 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 }
 
+func info(w http.ResponseWriter, r *http.Request) {
+	content := fmt.Sprint("Method: ", r.Method, "\n",
+		"URL: ", r.URL, "\n",
+		"URI: ", r.RequestURI, "\n",
+		"Protocol: ", r.Proto, "\n",
+		"Header: ", r.Header, "\n",
+		"Body: ", r.Body)
+	io.WriteString(w, content)
+}
+
 func main() {
 	http.HandleFunc("/", hello)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/info", info)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
